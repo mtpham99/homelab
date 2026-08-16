35a92640af7cdbbf482d14763b4c387841ba791c
Matthew T. Pham <pham.matthew+git@protonmail.com>
2026-08-16 14:20:42 -0500

    feat(tf-network): configure 'rtr01' and 'sw01' using new module

---
+++ terraform/network/rtr01/main.tf
@@ -0,0 +1,66 @@
+locals {
+  vlans = {
+    mgmt        = { id = 10, svi = "10.137.10.1/24" }
+    ceph-public = { id = 41, svi = "10.137.41.1/24" }
+    hosts       = { id = 50, svi = "10.137.50.1/24" }
+    home        = { id = 100, svi = "10.137.100.1/24" }
+    guest       = { id = 200, svi = "10.137.200.1/24" }
+  }
+
+  sw01_vlans = ["mgmt", "ceph-public", "hosts"]
+
+  interfaces = {
+    ether1 = {
+      comment  = "isp-uplink"
+      bridged  = false
+      tagged   = []
+      untagged = null
+    }
+
+    ether2 = {
+      comment  = "mgmt"
+      tagged   = []
+      untagged = "mgmt"
+    }
+
+    ether3 = {
+      comment  = "fs105-eth5-uplink"
+      tagged   = []
+      untagged = "mgmt"
+    }
+
+    ether4 = {
+      comment  = "sw01-sfpp7-uplink"
+      bridged  = false
+      tagged   = local.sw01_vlans
+      untagged = null
+    }
+
+    ether5 = {
+      comment  = "sw01-sfpp8-uplink"
+      bridged  = false
+      tagged   = local.sw01_vlans
+      untagged = null
+    }
+  }
+
+  bonds = {
+    "eth[4-5]" = {
+      comment  = "sw01-sfpp[7-8]-uplink"
+      tagged   = local.sw01_vlans
+      untagged = null
+
+      slaves               = ["ether4", "ether5"]
+      mode                 = "802.3ad"
+      transmit_hash_policy = "layer-3-and-4"
+      lacp_rate            = "1sec"
+    }
+  }
+}
+
+module "interfaces" {
+  source     = "../modules/mikrotik-interfaces"
+  vlans      = local.vlans
+  interfaces = local.interfaces
+  bonds      = local.bonds
+}

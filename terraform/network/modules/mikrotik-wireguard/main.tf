b1ed1f53741ccdbce740413fe8c42ac3f125fa15
Matthew T. Pham <pham.matthew+git@protonmail.com>
2026-08-22 13:44:36 -0500

    feat(tf-network): add 'mikrotik-wireguard' module
    Added new 'mikrotik-wireguard' module for managing / configuring
    wireguard server and associated peers as terraform resources.

---
+++ terraform/network/modules/mikrotik-wireguard/main.tf
@@ -0,0 +1,35 @@
+# --- wireguard server ---
+
+resource "routeros_interface_wireguard" "wg_server" {
+  comment     = var.name
+  name        = var.name
+  listen_port = var.port
+}
+
+resource "routeros_ip_address" "wg_server" {
+  comment   = routeros_interface_wireguard.wg_server.comment
+  interface = routeros_interface_wireguard.wg_server.name
+  address   = "${cidrhost(var.subnet, 1)}/${split("/", var.subnet)[1]}"
+}
+
+
+# --- wireguard peers ---
+
+resource "routeros_interface_wireguard_peer" "wg_peers" {
+  for_each = var.peers
+
+  comment         = "${each.key}-${routeros_interface_wireguard.wg_server.name}"
+  name            = "${each.key}-${routeros_interface_wireguard.wg_server.name}"
+  interface       = routeros_interface_wireguard.wg_server.name
+  public_key      = each.value.public_key
+  preshared_key   = each.value.preshared_key
+  allowed_address = ["${each.value.peer_address}/32"]
+
+  client_dns      = each.value.peer_dns
+  client_address  = "${each.value.peer_address}/32"
+  client_endpoint = var.endpoint
+  # TODO: pr #987 https://github.com/terraform-routeros/terraform-provider-routeros/pull/987
+  # client_allowed_address = each.value.peer_allowed_address
+
+  is_responder = true
+}
---
+++ terraform/network/modules/mikrotik-wireguard/variables.tf
@@ -0,0 +1,68 @@
+# --- wireguard server ---
+
+variable "name" {
+  description = "Interface name for wireguard server"
+  type        = string
+}
+
+variable "port" {
+  description = "wireguard udp port"
+  type        = number
+  default     = 51820
+
+  validation {
+    condition     = 0 < var.port && var.port <= 65535
+    error_message = "port number must be in the range (0, 65535]"
+  }
+}
+
+variable "subnet" {
+  description = "tunnel subnet in cidr notation"
+  type        = string
+
+  validation {
+    condition     = can(cidrhost(var.subnet, 0))
+    error_message = "invalid cidr string"
+  }
+}
+
+variable "endpoint" {
+  description = "wan endpoint for wireguard server peers will connect to"
+  type        = string
+  sensitive   = true
+}
+
+
+# --- wireguard peers ---
+
+variable "peers" {
+  description = "wireguard peer definitions"
+
+  type = map(object({
+    public_key    = string
+    preshared_key = optional(string)
+    peer_address  = string
+    peer_dns      = optional(string)
+    # TODO: pr #987: https://github.com/terraform-routeros/terraform-provider-routeros/pull/987
+    # peer_allowed_address = optional(list(string))
+  }))
+
+  default = {}
+
+  validation {
+    condition = alltrue([
+      for peer_k, peer_v in var.peers : can(cidrhost("${peer_v.peer_address}/32", 0))
+    ])
+    error_message = "invalid ip address string"
+  }
+
+  validation {
+    condition = alltrue([
+      for peer_k, peer_v in var.peers : (
+        peer_v.peer_dns == null ||
+        can(cidrhost("${peer_v.peer_dns}/32", 0))
+      )
+    ])
+    error_message = "invalid ip address string"
+  }
+}

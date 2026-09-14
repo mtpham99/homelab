6ded331324c1118e5066aafdc64bf60aa7e31827
Matthew T. Pham <pham.matthew+git@protonmail.com>
2026-09-13 21:07:08 -0500

    feat(nix-hci): add 'hci.disks' nix module and partition scheme with disko

--- nix/hosts/hci02/default.nix
+++ nix/hosts/hci02/default.nix
@@ -11,6 +11,8 @@
     # hardware configuration via nixos-facter
     # hardware.facter.reportPath = ./hci02-facter.json;
 
+    hci.disks.osDisk = "/dev/disk/by-path/pci-0000:58:00.0-nvme-1";
+
     # state version
     # see: https://wiki.nixos.org/wiki/FAQ/When_do_I_update_stateVersion
     system.stateVersion = "26.05";
